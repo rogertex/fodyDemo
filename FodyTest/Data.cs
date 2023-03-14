0f981cdeaf66519d466b7c1c083a8a11a259a100
roger <qunlin.gu@163.com>
2023-03-14 16:38:22 +0800

    添加项目文件。

---
+++ FodyTest/Data.cs
@@ -0,0 +1,44 @@
+﻿
+using PropertyChanged;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace FodyTest
+{
+
+    //public class Data:INotifyPropertyChanged
+    //{
+    //    private string name;
+
+    //    public string Name
+    //    {
+    //        get { return name; }
+    //        set { name = value; OnPropertyChanged("Name"); }
+    //    }
+
+    //    public int Age { get; set; }
+
+    //    public event PropertyChangedEventHandler PropertyChanged;
+
+    //    private void OnPropertyChanged(string propertyName = "")
+    //    {
+    //        if (PropertyChanged != null)
+    //        {
+    //            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+    //        }
+    //    }
+    //}\
+
+    [AddINotifyPropertyChangedInterface]
+    public class Data
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
---
+++ FodyTest/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FodyTest
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Data dt = new Data()
+        {
+            Name = "dd",
+            Age = 12,
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+            DataContext = dt;
+        }
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+             dt.Name= "xx";
+       
+        }
+    }
+}
